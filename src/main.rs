e5a64ba77afd042d920c32e1fc274c502004d1b1
Your Name here only <krishnsundaram@gmail.com>
2024-02-12 20:24:48 +0530

    testing speed method

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,10 @@ use crate::connectivity::{Connectivity, WifiConnectionError};
 use crate::platforms::{Config, WiFi};
 
 use std::process::{Command, Stdio};
-use std::str;
 
 fn main() -> Result<(), WifiConnectionError> {
     let config = Some(Config {
-        interface: Some("wlo1"),
+        interface: Some("wlan0"),
     });
 
     let mut wifi = WiFi::new(config);
@@ -27,28 +26,8 @@ fn main() -> Result<(), WifiConnectionError> {
         ),
         Err(err) => println!("The following error occurred: {:?}", err),
     }
-
-    let output = Command::new("nmcli")
-        .arg("dev")
-        .arg("wifi")
-        .arg("list")
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("nothing");
-    // nmcli --fields RATE device wifi list
-    let awk = Command::new("awk")
-        .arg(r"/\*/{if (NR!=1) {print $6}}")
-        .stdin(Stdio::from(output.stdout.unwrap())) // Pipe through.
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
-
-    let result = awk.wait_with_output().unwrap();
-    let result = str::from_utf8(&result.stdout).unwrap();
-    println!("{}Mbit/s", result);
-
-
-
+    let speed = wifi.speed().unwrap();
+    println!("Speed is: {}", speed);
 
     Ok(())
 }
